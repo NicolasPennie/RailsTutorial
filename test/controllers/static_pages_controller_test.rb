922aa8e403fde92f0ab328f385fe1a5870842072
Nicolas <nicolas.pennie@gmail.com>
2018-07-25 03:30:28 +0000

    Update root and finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,11 @@ def setup
   @base_title = "Ruby on Rails Tutorial Sample App"
 end 
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -27,6 +32,6 @@ end
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "contact", "Contact | #{@base_title}"
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
