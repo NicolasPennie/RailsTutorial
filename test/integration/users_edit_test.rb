145b955c1910693e0aef23999f8328278470b0b7
Nicolas <nicolas.pennie@gmail.com>
2018-08-17 15:07:31 +0000

    Add edit validations

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "unsuccessful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name: "",
+                                              email: "foo@invalid",
+                                              password: "foo",
+                                              password_confirmation: "bar" } }
+    assert_template 'users/edit'
+    assert_select "div#error_explanation" do
+      assert_select "li", count: 4
+    end
+  end
+  
+  test "successful edit" do
+    name = "Mike"
+    email = "mike@valid.com"
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?, "test for empty flash"
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+end
