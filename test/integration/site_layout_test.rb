2caa95d5c496eef6bd32a3d145c77e1d42d73769
Nicolas <nicolas.pennie@gmail.com>
2018-08-29 23:50:46 +0000

    Test logged-in site layout

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
+require 'sessions_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  
+  def setup 
+    @user = users(:michael)
+  end
   
   test "layout links" do
     get root_path
@@ -11,10 +16,29 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
     
+    get help_path
+    assert_template 'static_pages/help'
+    
+    get about_path
+    assert_template 'static_pages/about'
+    
+    get contact_path
+    assert_template 'static_pages/contact'
+    
     get signup_path
     assert_template 'users/new'
-    
   end
   
-  
+  test "signed-in layout links" do
+    log_in_as(@user)
+    get root_path   
+        assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path, "Users"
+    assert_select "a[href=?]", user_path(@user), "Profile"
+    assert_select "a[href=?]", edit_user_path(@user), "Settings"
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,8 +18,9 @@ class ActiveSupport::TestCase
   def log_in_as(user)
     session[:user_id] = user.user_id
   end
+end
   
-  class ActionDispatch::IntegrationTest
+class ActionDispatch::IntegrationTest
     
     # Log in as a particular user.
     def log_in_as(user, password: 'password', remember_me: '1')
@@ -27,5 +28,4 @@ class ActiveSupport::TestCase
                                             password: password,
                                             remember_me: remember_me } }
     end
-  end
 end
