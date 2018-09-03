f655ae3ca8796ca47ad7a9f664f4a8784a3a58e0
Nicolas <nicolas.pennie@gmail.com>
2018-09-03 20:45:22 +0000

    Generate account activation controller and mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token
 	
-	before_save { email.downcase! }
+	before_save :email_downcase
+	before_create :create_activation_digest
+	
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, 
 										length: { maximum: 255 }, 
@@ -41,4 +43,15 @@ class User < ApplicationRecord
     return false if remember_digest.nil?
   	BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+  
+  private
+  
+    def email_downcase
+      email.downcase!
+    end
+    
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
