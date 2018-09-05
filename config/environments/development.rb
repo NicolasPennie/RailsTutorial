28a48c44bca90033b3e476a545a33334712d0768
Nicolas <nicolas.pennie@gmail.com>
2018-09-05 00:07:19 +0000

    configured mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,10 +25,11 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'https://af4fc51a20c8441aa0875aca6a435f77.vfs.cloud9.us-east-2.amazonaws.com/'     # Cloud IDE
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
