6d8da2d8df8b4310edeca92a69f2633c117793af
Nicolas <nicolas.pennie@gmail.com>
2018-11-15 00:44:10 +0000

    Enable following

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
 	has_many :active_relationships, class_name: "Relationship",
 	                                foreign_key: "follower_id", 
 	                                dependent: :destroy
+	has_many :following, through: :active_relationships, source: :followed
 	
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, 
@@ -79,6 +80,18 @@ class User < ApplicationRecord
   
   def feed
     Micropost.where("user_id = ?", id)
+  end
+  
+  def follow(other_user)
+    following << other_user
+  end
+  
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+  
+  def following?(other_user)
+    following.include?(other_user)
   end
   
   private
