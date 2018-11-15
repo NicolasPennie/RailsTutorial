49954b3dbb758badcbdb60dc891ce4efc25c46ff
Nicolas <nicolas.pennie@gmail.com>
2018-11-15 00:16:05 +0000

    Add relationships model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
 	before_create :create_activation_digest
 	
 	has_many :microposts, dependent: :destroy
+	has_many :active_relationships, class_name: "Relationship",
+	                                foreign_key: "follower_id", 
+	                                dependent: :destroy
+	
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, 
 										length: { maximum: 255 }, 
